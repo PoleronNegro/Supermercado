8296d551240ae3332da1d355a810926313ba4d54
PoleronNegro <os.torress@alumnos.duoc.cl>
2019-11-26 15:08:16 -0300

    modelo

--- supermercado/models.py
+++ supermercado/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+class Genero(models.Model):
+    datalle = models.CharField(max_length=15,null=False,blank=False)
+
+class Region(models.Model):
+    nombre_region = models.CharField(max_length=64,null=False,blank=False)
+    num_regional = models.CharField(max_length=3,null=False,blank=False)
+
+class Provincia(models.Model):
+    nombre_provincia = models.CharField(max_length=64,null=False,blank=False)
+    region = models.ForeignKey(Region,on_delete = models.CASCADE)
+
+class Comuna(models.Model):
+    nombre_comuna = models.CharField(max_length=64,null=False,blank=False)
+    provincia = models.ForeignKey(Provincia,on_delete = models.CASCADE)
+
+class Persona(models.Model):
+    Run = models.CharField(primary_key=True,max_length=15,null=False,blank=False)
+    nombre = models.CharField(max_length=25,null=False,blank=False)
+    apellido = models.CharField(max_length=25,null=False,blank=False)
+    genero = models.ForeignKey(Genero,on_delete=models.CASCADE)
+    correo = models.CharField(max_length=50,null=False,blank=False)
+    direccion = models.CharField(max_length=50,null=False,blank=False)
+    comuna = models.ForeignKey(Comuna,on_delete=models.CASCADE)
+    telefono = models.CharField(max_length=12,null=False,blank=False)
+    tipo = models.CharField(max_length=20,null=False,blank=False)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=30,null=False,blank=False)
+    descripcion = models.TextField(max_length=500,null=False,blank=False)
+    stock = models.PositiveSmallIntegerField()
+    precio = models.IntegerField()
+
+class Boleta(models.Model):
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
+    cantidad = models.CharField(max_length=10,null=False,blank=False)
+    fecha = models.DateTimeField(auto_now=True)
+    total = models.PositiveIntegerField()
+
